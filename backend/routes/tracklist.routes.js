1ff16bffb72009d4884c549e831dfe0c992c33e7
Shuaib464 <shuaibshekh464@gmail.com>
2024-09-09 22:41:55 +0530

    add routes for application

---
+++ backend/routes/tracklist.routes.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+// make a router instance
+const router = express.Router();
+
+// import controller
+const {createTodo} = require('../controllers/createTodo');
+const {getTodosByDay, getTodoById} = require('../controllers/getTodo');
+const {updateTodo} = require('../controllers/updateTodo');
+const {deleteTodo} = require('../controllers/deleteTodo');
+const {createDay} = require('../controllers/createDay')
+const {getMonth} = require('../controllers/getMonth')
+
+
+// map the route with the controller
+router.post('/createTodo', createTodo)
+router.get('/getTodosByDay/:dayId', getTodosByDay);
+router.get('/getTodoById/:id', getTodoById);
+router.put('/updateTodo/:id', updateTodo)
+router.delete('/deleteTodo/:id', deleteTodo)
+
+router.post('/createDay', createDay)
+router.get('/getMonth', getMonth)
+
+
+// export the router 
+module.exports = router;
