bff62701ab2f759d24ef0d995fb2b0d29c4f6f6d
Shuaib464 <shuaibshekh464@gmail.com>
2024-09-09 22:41:39 +0530

    add index.js

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+// create instance of server
+const app = express();
+
+//load config from env file
+require('dotenv').config()
+
+// define port no for server
+const PORT = process.env.PORT || 4000;
+
+// middleware to parse req body
+app.use(express.json());
+
+// mount the todo api routes
+const tracklistRoutes = require('./routes/tracklist.routes')
+app.use('/api/v1', tracklistRoutes);
+
+// Live the server at given Port 
+app.listen(PORT, () => {
+    console.log(`App is running at PORT ${PORT} successfully`);
+})
+
+// connect with DB
+const dbConnect = require('./config/database')
+dbConnect();
