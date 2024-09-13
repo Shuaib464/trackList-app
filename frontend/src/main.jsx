fb2f7b0defcdc87d74ef7d206bc8455a7a49fea0
Shuaib464 <shuaibshekh464@gmail.com>
2024-09-13 19:26:50 +0530

    register store and define routing

---
+++ frontend/src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
+import Todo from './pages/Todo.jsx'
+import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/todo/:dayId',
+        element: <Todo />
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>,
+)
