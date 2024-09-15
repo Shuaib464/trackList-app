650c0c78de9af72149135d9183cd4d3c37ed688c
Shuaib464 <shuaibshekh464@gmail.com>
2024-09-15 22:49:59 +0530

    add css for day-specific

--- frontend/src/components/DayCard.jsx
+++ frontend/src/components/DayCard.jsx
@@ -1,17 +1,21 @@
 import { Link  } from "react-router-dom"
 
-function DayCard({id, day, todos, completedTodos}) {
+function DayCard({id, day, month, year, todos, completedTodos}) {
   // console.log("day in dayCArd -- ",day)
+  const date = new Date()
+  const currentDay = date.getDate();
+  const currentMonth = date.getMonth()
+  const currentYear = date.getFullYear()
 
 
   return (
     <div>
       <Link to={`/todo/${id}`}>
-        <button className={`ml-3 p-2 mt-3 rounded-2xl text-black text-xl w-[45px] text-center font-bold
-                   ${todos.length <= 0 ? ('bg-slate-50') : (todos.length === completedTodos.length ? ('bg-green-300') : ('bg-red-200'))} 
-                  border border-red-500 cursor-pointer shadow-md
-                  transition ease-in-out delay-100 hover:translate-y-1 hover:scale-110 duration-300
-                  hover:bg-rose-200 hover:text-red-500`}
+        <button className={`ml-3 p-2 mt-3 rounded-2xl border border-red-500 cursor-pointer shadow-md text-xl w-[45px]
+                  ${(currentDay === day) && (currentMonth === month) && (currentYear === year) ? ('text-red-600 scale-125 shadow-red-400') : ('text-black')} 
+                  ${todos.length <= 0 ? ('bg-slate-50') : (todos.length === completedTodos.length ? ('bg-green-300') : ('bg-red-200'))} 
+                  text-center font-bold transition ease-in-out delay-100 hover:translate-y-1 hover:scale-110 duration-300
+                hover:bg-rose-200 hover:text-red-500`}
                   >
             {day}
         </button>
