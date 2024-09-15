da2020051218e41a2a6a1fdcf931409e1cb7e338
Shuaib464 <shuaibshekh464@gmail.com>
2024-09-15 22:54:47 +0530

    add fetchMonthByDate asyncThunk

--- frontend/src/store/features/monthSlice.js
+++ frontend/src/store/features/monthSlice.js
@@ -10,9 +10,24 @@ export const fetchMonth = createAsyncThunk('month/fetchMonth', async () => {
     return response.data.data;
 })
 
+// fetch month and days by date
+export const fetchMonthByDate = createAsyncThunk('month/fetchMonthByDate', async ({month, year}) => {
+    console.log('In fetchMonthByDate slice func...', month)
+    const params = {
+        month: month,
+        year: year,
+    }
+    let response = await axios.get('/api/v1/getMonthByDate', {
+                                                params: params
+                                            });
+    console.log("fetchMonth (Month_slice) ",response.data.data);
+    
+    return response.data.data;
+})
+
 //define initial state
 const initialState = {
-    month:'', arrDays: [], loading:false, error:''
+    month:null,monthName:'',monthId:null, year:null, arrDays: [], loading:false, error:''
 }
 
 const monthSlice = createSlice({
@@ -31,11 +46,35 @@ const monthSlice = createSlice({
                 state.loading = false
                 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
                     'August', 'September', 'October', 'November', 'December']
-                state.month = months[action.payload.month]
+                state.monthName = months[action.payload.month]
+                state.monthId = action.payload._id;
+                state.month = action.payload.month
                 state.arrDays = action.payload.days;
+                state.year = action.payload.year
                 state.error = ''
             })
             .addCase(fetchMonth.rejected, (state, action) => {
+                state.loading = false,
+                state.error = action.payload
+            })
+
+        builder
+            .addCase(fetchMonthByDate.pending, (state) => {
+                state.loading = true;
+                state.error = '';
+            })
+            .addCase(fetchMonthByDate.fulfilled, (state,action) => {
+                state.loading = false
+                const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
+                    'August', 'September', 'October', 'November', 'December']
+                state.monthName = months[action.payload.month]
+                state.monthId = action.payload._id;
+                state.month = action.payload.month
+                state.arrDays = action.payload.days;
+                state.year = action.payload.year
+                state.error = ''
+            })
+            .addCase(fetchMonthByDate.rejected, (state, action) => {
                 state.loading = false,
                 state.error = action.payload
             })
