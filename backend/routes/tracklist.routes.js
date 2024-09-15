ae2edfe9e3576b9fa7beb41ac00702640984f16d
Shuaib464 <shuaibshekh464@gmail.com>
2024-09-15 22:48:44 +0530

    map handler with routes

--- backend/routes/tracklist.routes.js
+++ backend/routes/tracklist.routes.js
@@ -9,7 +9,7 @@ const {getTodosByDay, getTodoById} = require('../controllers/getTodo');
 const {updateTodo} = require('../controllers/updateTodo');
 const {deleteTodo} = require('../controllers/deleteTodo');
 const {createDay} = require('../controllers/createDay')
-const {getMonth} = require('../controllers/getMonth')
+const {getMonth, getMonthByDate, getMonthById} = require('../controllers/getMonth')
 
 
 // map the route with the controller
@@ -21,7 +21,8 @@ router.delete('/deleteTodo/:id', deleteTodo)
 
 router.post('/createDay', createDay)
 router.get('/getMonth', getMonth)
-
+router.get('/getMonthByDate', getMonthByDate)
+router.get('/getMonthById/:monthId', getMonthById)
 
 // export the router 
 module.exports = router;
