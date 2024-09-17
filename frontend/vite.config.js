28e91092987b38cd97430e080031071d25304b96
Shuaib464 <shuaibshekh464@gmail.com>
2024-09-17 16:56:46 +0530

    change the backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://tracklist-app-backend.onrender.com',
+      '/api': 'https://tracklist-backend.onrender.com',
     },
   },
   plugins: [react()],
