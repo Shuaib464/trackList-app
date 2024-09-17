ba218671d8c927f194bd31448059292d39c8867a
Shuaib464 <shuaibshekh464@gmail.com>
2024-09-17 19:31:28 +0530

    add proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     '/api': "https://tracklist-backend.onrender.com/api",
-  //   },
-  // },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://tracklist-backend.onrender.com',
+      }
+    },
+  },
   plugins: [react()],
 })
